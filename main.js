714fe6de5a723415dbd83661a1a20c2fbc0118af
Jordit <jordidiaz2020@gmail.com>
2025-01-22 13:12:50 -0500

    Create guess pokemon game

--- main.js
+++ main.js
@@ -1,31 +1,18 @@
-import { renderMystery, renderOptions } from "./src/game-render-functions";
-import { getRandomPokemon } from "./src/fetch-functions";
-import { checkWin } from "./src/game-logic-functions";
+import { renderGame } from "./src/game-render-functions";
 
 const main = async () => {
-  const modal = document.getElementById("guess-pokemon-modal");
-  const openDialog = document.getElementById("dialog-open-button");
-  const closeDialog = document.getElementById("dialog-close-button");
-  const optionsDiv = document.getElementById("options-list");
+  const gameDialog = document.getElementById("game-dialog");
+  const openDialogButton = document.getElementById("dialog-open-button");
+  const closeDialogButton = document.getElementById("dialog-close-button");
 
-  const sampleData = await getRandomPokemon();
-
-  renderMystery(modal, sampleData);
-  renderOptions(optionsDiv, sampleData);
-
-  openDialog.addEventListener("click", () => {
-    modal.showModal();
+  openDialogButton.addEventListener("click", () => {
+    document.getElementById("game-content").innerHTML = "";
+    gameDialog.showModal();
+    renderGame();
   });
 
-  closeDialog.addEventListener("click", () => {
-    modal.close();
-  });
-
-  optionsDiv.addEventListener("click", (event) => {
-    const guessId = Number(event.target.id);
-    if (guessId) {
-      checkWin(guessId, sampleData.id);
-    }
+  closeDialogButton.addEventListener("click", () => {
+    gameDialog.close();
   });
 };
 
--- src/game-render-functions.js
+++ src/game-render-functions.js
@@ -1,26 +1,85 @@
 import { getRandomPokemon } from "./fetch-functions";
 
-export const renderMystery = (modal, pokemon) => {
-  const img = document.createElement("img");
-  img.src = pokemon.img;
-  modal.append(img);
+let pokemon;
+const gameContainer = document.getElementById("game-content");
+const dialogHeader = document.getElementById("results");
+const dialogFooter = document.getElementById("game-footer");
+
+const handlePlayAgain = () => {
+  gameContainer.innerHTML = "";
+  renderGame();
 };
 
-export const renderOptions = async (Ul, correctPokemon) => {
+const guessHandler = (event) => {
+  const guessId = Number(event.target.id);
+  if (guessId) {
+    checkWin(guessId, pokemon.id);
+  }
+};
+
+const checkWin = (idGuess, correctId) => {
+  const text = document.createElement("p");
+  if (idGuess === correctId) {
+    text.textContent = `It's ${pokemon.name}!`;
+    text.style.color = "green";
+  } else {
+    text.textContent = `Oh no! it was ${pokemon.name}`;
+    text.style.color = "red";
+  }
+  document.querySelector("#image-container img").style.filter = "none";
+  dialogHeader.append(text);
+  renderPlayAgain();
+};
+
+export const renderPlayAgain = () => {
+  document
+    .getElementById("options-list")
+    .removeEventListener("click", guessHandler);
+  const playAgainButton = document.createElement("button");
+  playAgainButton.id = "play-again-button";
+  playAgainButton.textContent = "Play Again?";
+  playAgainButton.addEventListener("click", handlePlayAgain);
+  dialogFooter.append(playAgainButton);
+};
+
+export const renderOptions = async (correctPokemon) => {
+  const list = document.createElement("ul");
+  list.id = "options-list";
+  list.addEventListener("click", guessHandler);
+
   const card = document.createElement("li");
   card.innerText = correctPokemon.name;
   card.id = correctPokemon.id;
+
   for (let i = 0; i < 2; i += 1) {
     const pokemon = await getRandomPokemon();
     const wrongCard = document.createElement("li");
     wrongCard.id = pokemon.id;
     wrongCard.innerText = pokemon.name;
-    Ul.append(wrongCard);
+    list.append(wrongCard);
   }
-  Ul.append(card);
 
-  //Shuffling
-  for (let i = 0; i < Ul.children.length; i += 1) {
-    Ul.appendChild(Ul.children[(Math.random() * i) | 0]);
+  list.append(card);
+
+  //Shuffle
+  for (let i = 0; i < list.children.length; i += 1) {
+    list.appendChild(list.children[(Math.random() * i) | 0]);
   }
+  dialogFooter.append(list);
+};
+
+export const renderGame = async () => {
+  dialogFooter.innerHTML = "";
+  dialogHeader.innerHTML = "";
+  pokemon = await getRandomPokemon();
+
+  const div = document.createElement("div");
+  div.id = "image-container";
+
+  const img = document.createElement("img");
+  img.src = pokemon.img;
+
+  div.append(img);
+  gameContainer.append(div);
+  renderOptions(pokemon);
 };
